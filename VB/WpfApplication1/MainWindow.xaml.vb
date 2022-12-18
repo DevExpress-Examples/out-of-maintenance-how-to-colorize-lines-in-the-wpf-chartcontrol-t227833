524d1347a7331d672d209d0e463e3f7da82f370b
DevExpressExampleBot <fakeEmail>
2022-12-18 16:09:43 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -1,46 +1,44 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.Data
-Imports System.Linq
 Imports System.Windows
-Imports System.Windows.Media
-Imports System.Windows.Shapes
-Imports DevExpress.Xpf.Charts
 
 Namespace WpfApplication1
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-			UpdateDataSource()
-			InitializeChartControl()
-		End Sub
-		Private Function CreateData(ByVal rows As Integer) As DataTable
-			Dim r As New Random()
+        Public Sub New()
+            Me.InitializeComponent()
+            UpdateDataSource()
+            InitializeChartControl()
+        End Sub
 
-			Dim dt As New DataTable()
-			dt.Columns.Add("Ser", GetType(Integer))
-			dt.Columns.Add("Arg", GetType(Integer))
-			dt.Columns.Add("Value", GetType(Integer))
-			For i As Integer = 0 To rows - 1
-				dt.Rows.Add(0, i, r.Next(100))
-				dt.Rows.Add(1, i, r.Next(100))
-			Next i
-			Return dt
-		End Function
-		Private Sub InitializeChartControl()
-			chartControl1.Diagram.Series.Clear()
-			chartControl1.Diagram.SeriesDataMember = "Ser"
-			chartControl1.Diagram.SeriesTemplate.ValueDataMember = "Value"
-			chartControl1.Diagram.SeriesTemplate.ArgumentDataMember = "Arg"
-		End Sub
-		Private Sub UpdateDataSource()
-			chartControl1.DataSource = CreateData(10)
-		End Sub
-	End Class
+        Private Function CreateData(ByVal rows As Integer) As DataTable
+            Dim r As Random = New Random()
+            Dim dt As DataTable = New DataTable()
+            dt.Columns.Add("Ser", GetType(Integer))
+            dt.Columns.Add("Arg", GetType(Integer))
+            dt.Columns.Add("Value", GetType(Integer))
+            For i As Integer = 0 To rows - 1
+                dt.Rows.Add(0, i, r.Next(100))
+                dt.Rows.Add(1, i, r.Next(100))
+            Next
+
+            Return dt
+        End Function
+
+        Private Sub InitializeChartControl()
+            Me.chartControl1.Diagram.Series.Clear()
+            Me.chartControl1.Diagram.SeriesDataMember = "Ser"
+            Me.chartControl1.Diagram.SeriesTemplate.ValueDataMember = "Value"
+            Me.chartControl1.Diagram.SeriesTemplate.ArgumentDataMember = "Arg"
+        End Sub
+
+        Private Sub UpdateDataSource()
+            Me.chartControl1.DataSource = CreateData(10)
+        End Sub
+    End Class
 End Namespace
